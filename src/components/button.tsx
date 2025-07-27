84aa14d8ed835e0d7d264b6d485eba7725012976
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-27 18:00:56 -0300

    feat(ui): create Button component with variant and size support

---
+++ src/components/button.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+// Default icon size to use => 16
+
+const buttonVariants = cva(
+  "flex items-center gap-1 text-xs font-medium rounded-md",
+  {
+    variants: {
+      variant: {
+        default: "bg-foreground text-background hover:bg-foreground/90",
+        secondary: "bg-foreground/10 text-foreground hover:bg-foreground/15",
+        lime: "bg-lime-300 text-lime-950 hover:bg-lime-300/90",
+        destructive: "bg-red-400 text-background hover:bg-red-400/90",
+        outline: "bg-transparent text-foreground border border-border hover:bg-foreground/5"
+      },
+      size: {
+        default: "px-3 py-2",
+        icon: "p-2"
+      }
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default"
+    },
+  }
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size }), className)}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
