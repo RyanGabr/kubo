735ae2de9e82eb90a9b04b1862614ef9596c66c6
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-27 18:28:30 -0300

    feat(auth): create authentication page UI layout

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,8 @@
+import { Sign } from "@/pages/auth/sign-in";
 import { Home } from "@/pages/home";
 import { createBrowserRouter } from "react-router-dom";
 
-export const routes = createBrowserRouter([{ path: "/", element: <Home /> }]);
+export const routes = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "sign-in", element: <Sign /> },
+]);
