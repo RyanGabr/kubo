806aaf3e8de5601260c33227b9f88e72543939d9
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-27 16:58:22 -0300

    feat: add ThemeProvider and set up React Router

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./app/routes";
+import { ThemeProvider } from "./components/theme-provider";
+
 export function App() {
-  return "Setup project"
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={routes} />
+    </ThemeProvider>
+  );
 }
---
+++ src/app/routes.tsx
@@ -0,0 +1,4 @@
+import { Home } from "@/pages/home";
+import { createBrowserRouter } from "react-router-dom";
+
+export const routes = createBrowserRouter([{ path: "/", element: <Home /> }]);
