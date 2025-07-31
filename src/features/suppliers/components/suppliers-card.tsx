8bb6b38512068dc4fde7939609159652e00fc45d
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-31 09:47:34 -0300

    feat(suppliers): add profile picture with name initial on supplier card

--- src/features/suppliers/components/suppliers-card.tsx
+++ src/features/suppliers/components/suppliers-card.tsx
@@ -25,13 +25,18 @@ export function SuppliersCard({
     <ContextMenu>
       <ContextMenuTrigger>
         <div className="rounded-md border border-border bg-card flex flex-col hover:bg-foreground/7">
-          <div className="p-5 border-b">
-            <strong className="font-medium text-sm truncate text-ellipsis max-w-80">
-              {name}
-            </strong>
-            <p className="font-medium text-xs text-foreground/50 truncate text-ellipsis max-w-80">
-              {notes}
-            </p>
+          <div className="p-5 border-b flex items-center gap-3">
+            <div className="size-9 rounded-full bg-foreground/20 flex items-center justify-center">
+              {name.split("")[0]}
+            </div>
+            <div>
+              <strong className="font-medium text-sm truncate text-ellipsis max-w-80">
+                {name}
+              </strong>
+              <p className="font-medium text-xs text-foreground/50 truncate text-ellipsis max-w-80">
+                {notes}
+              </p>
+            </div>
           </div>
           <div className="px-5 py-3 flex items-center gap-2">
             <div className="px-2 py-1 border border-border rounded-md flex items-center gap-2 text-xs font-medium text-foreground/50">
