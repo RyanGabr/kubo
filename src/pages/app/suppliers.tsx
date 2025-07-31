34a5d1ac488505ce8904943430ddcd0395011d74
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-31 13:27:12 -0300

    chore(suppliers): update document title on mount

--- src/pages/app/suppliers.tsx
+++ src/pages/app/suppliers.tsx
@@ -1,7 +1,12 @@
 import { SuppliersHeader } from "@/features/suppliers/components/suppliers-header";
 import { SuppliersList } from "@/features/suppliers/suppliers-list";
+import { useEffect } from "react";
 
 export function Suppliers() {
+  useEffect(() => {
+    document.title = "Fornecedores | Kubo"
+  }, []);
+
   return (
     <div className="h-full flex flex-col">
       <SuppliersHeader />
