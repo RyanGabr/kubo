6b7d7604bdeab2315422077b5401e8bc316f6c1d
RyanGabr <ryanalexandregabriel@gmail.com>
2025-08-04 10:27:44 -0300

    chore(products): update document title on mount

--- src/pages/app/products.tsx
+++ src/pages/app/products.tsx
@@ -1,7 +1,12 @@
 import { ProductsHeader } from "@/features/products/components/products-header";
 import { ProductsList } from "@/features/products/products-list";
+import { useEffect } from "react";
 
 export function Products() {
+  useEffect(() => {
+    document.title = "Produtos | Kubo";
+  }, []);
+
   return (
     <div className="h-full flex flex-col">
       <ProductsHeader />
